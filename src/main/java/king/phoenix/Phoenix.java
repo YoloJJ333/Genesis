b4fd04307e78757c6fb8d0fb307d79b9da18fcd7
YoloJJ333 <107592764+YoloJJ333@users.noreply.github.com>
2022-11-08 19:26:20 -0800

    Initial Project Workspace

---
+++ src/main/java/king/phoenix/Phoenix.java
@@ -0,0 +1,10 @@
+package king.phoenix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Phoenix implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/king/phoenix/client/PhoenixClient.java
@@ -0,0 +1,13 @@
+package king.phoenix.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PhoenixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
